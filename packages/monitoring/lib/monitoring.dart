5a35456fe0debe30ca18056dea0fc651147272e7
mamong <chyy.meng@gmail.com>
2026-01-17 20:53:15 +0800

    update firebase and ios 15

--- packages/monitoring/lib/monitoring.dart
+++ packages/monitoring/lib/monitoring.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:monitoring/src/firebase_options.dart';
 
-export 'src/dynamic_link_service.dart';
+// export 'src/dynamic_link_service.dart';
 export 'src/analytics_service.dart';
 export 'src/error_reporting_service.dart';
 export 'src/explicit_crash.dart';
